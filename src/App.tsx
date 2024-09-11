9c1d0502c72780ceb47ab3922928ff22035ee71b
mrdhanz <dandigilangr99@gmail.com>
2024-09-11 17:26:19 +0700

    test update code

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactJS
+          Learn ReactJS with TypeScript
         </a>
       </header>
     </div>
